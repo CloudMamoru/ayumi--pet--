d4c0eaf119c5aab6d11f8c4309e95f284025b1fe
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-11-09 10:57:33 +0300

    functions (loadData, saveData) have been implemented

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,21 @@
 'use strict'
 
-document.querySelector('.btn').addEventListener('click', function () {
-  console.log(this)
-})
+let habits = [];
+const HABIT_KEY = 'HABIT_KEY';
+
+
+/* utils */
+function loadData() {
+  const habitsString = localStorage.getItem(HABIT_KEY);
+  const habitsArray = JSON.parse(habitsString);
+  if (Array.isArray(habitsArray)) habits = habitsArray;
+} 
+
+function saveData() {
+  localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
+} 
+
+/* Init data */
+(() => {
+  loadData();
+})()
